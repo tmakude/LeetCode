570e46c497c9c56beb982a329a77e47f8ef2d0bf
Tushar Makude <147398213+tmakude@users.noreply.github.com>
2024-08-25 15:03:12 +0530

    Time: 2 ms (52.08%), Space: 41.8 MB (16.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,66 +1,31 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for(int i = 0 ; i < s.length() ; i++)
-        {
-            if(st.size()==0)
-            {
-                st.push(s.charAt(i));
+
+        Stack<Character > st = new Stack<>();
+
+        for(char c : s.toCharArray()){
+
+            if( c == '(' || c == '{' || c == '['){
+
+                st.push(c);
             }
-            else if(s.charAt(i)=='(' || s.charAt(i)== '{' || s.charAt(i)== '[')
-            {
-                st.push(s.charAt(i));
-            }
-            else
-            {
-                if(s.charAt(i)==')')
-                {
-                    if(st.peek()=='(')
-                    {
-                        st.pop();
-                    }
-                    else
-                    {
-                        return false;
-                    }
+            else{
 
-                }
-                else if (s.charAt(i)=='}')
-                {
-                    if(st.peek()=='{')
-                    {
-                        st.pop();
-                    }
-                    else
-                    {
-                        return false;
-                    }
-
-                }
-                else
-                {
-                    if(st.peek()=='[')
-                    {
-                        st.pop();
-                    }
-                    else
-                    {
-                        return false;
-                    }
-
+                if(st.isEmpty()){
+                    return false;
                 }
 
+                char top = st.pop();
+
+                if((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[') ){
+
+                    return false;
+                }
+               
             }
         }
 
-        if(st.size()==0)
-        {
-            return true;
-        }
-        return false;
-
-
-
+        return st.isEmpty();
         
     }
 }
