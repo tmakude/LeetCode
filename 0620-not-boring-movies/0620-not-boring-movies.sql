65445d66460d03b3c6604bc7413bed6027f1cd80
Tushar Makude <147398213+tmakude@users.noreply.github.com>
2025-12-23 10:10:24 +0530

    Time: 216 ms (84.89%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,3 +1,5 @@
-select * from Cinema 
-where id % 2 != 0 and description != 'boring' 
-order by rating desc
+SELECT
+*
+FROM Cinema
+WHERE id % 2 != 0 AND Description != "Boring"
+ORDER BY rating DESC
